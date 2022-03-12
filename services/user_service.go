d5fc58472b42d421c69e66dcf0cee912e3ddda1a
Cláudio Santos <claudiosantos@sistnet.com.br>
2022-03-12 01:23:15 -0300

    feat: client streaming

--- services/user_service.go
+++ services/user_service.go
@@ -3,11 +3,14 @@ package services
 import (
 	"context"
 	"fmt"
+	"io"
+	"log"
 	"time"
 
 	"github.com/claudiojsantos/gRPC/pb"
 )
 
+// AddUsers(ctx context.Context, opts ...grpc.CallOption) (UserService_AddUsersClient, error)
 type UserService struct {
 	pb.UnimplementedUserServiceServer
 }
@@ -65,4 +68,30 @@ func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVe
 	time.Sleep(time.Second * 3)
 
 	return nil
+}
+
+func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
+	users := []*pb.User{}
+
+	for {
+		req, err := stream.Recv()
+
+		if err == io.EOF {
+			return stream.SendAndClose((&pb.Users{
+				User: users,
+			}))
+		}
+
+		if err != nil {
+			log.Fatalf("Error receiving stream: %v", err)
+		}
+
+		users = append(users, &pb.User{
+			Id:    req.GetId(),
+			Name:  req.GetName(),
+			Email: req.GetEmail(),
+		})
+
+		fmt.Println("Adding", req.GetName())
+	}
 }
